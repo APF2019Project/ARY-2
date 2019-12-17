8e5c5b4b2229bcc30408a26f07e7721e1f7d2950
amirhossein <alimohammadiamirhossein@gmail.com>
2019-12-17 14:59:07 +0330

    zombie change

--- src/Model/Card/Zombie.java
+++ src/Model/Card/Zombie.java
@@ -3,7 +3,7 @@ package Model.Card;
 public class Zombie extends Card {
     private int speed;
     private int shieldHealth;
-
+    private boolean startDelay;
 
     public int getSpeed() {
         return speed;
