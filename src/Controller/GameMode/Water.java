ba4c5cef82837a99f2097f04d8cf22e826a75a84
zombieJson <R3zadlt@gmail.com>
2020-01-01 16:12:00 +0330

    add some methods to water mode

--- src/Controller/GameMode/Water.java
+++ src/Controller/GameMode/Water.java
@@ -6,13 +6,18 @@ import Model.Card.Bullet;
 import Model.Card.Card;
 import Model.Card.Plant;
 import Model.Card.Zombie;
+import Model.Collection;
 import Model.Map.Map;
+import Model.Map.Square;
 import Model.Primary;
+import Exception.NotPlantException;
+import Exception.noCardSelected;
+import Exception.invalidCardExeption;
 
 import java.util.ArrayList;
 import java.util.Random;
 
-public class Water {
+public class Water implements GameMode {
     private Player player;
     private Map map;
     private int turn;
@@ -37,4 +42,108 @@ public class Water {
     public Map generateMap() {
         return Map.generateWater();
     }
+
+    @Override
+    public void showHand() throws NotPlantException {
+        for (int i = 0; i < selectedCards.size(); i++) {
+            Plant plant = (Plant)selectedCards.get(i);
+            System.out.println(selectedCards.get(i).getName() + " "
+                    +plant.getSun() + " " + plant.getTimeToReset());
+        }
+    }
+
+    @Override
+    public void select(String a) throws NotPlantException {
+        int index = Collection.findCard(a, selectedCards);
+        if(index != -1) {
+            Plant plant = (Plant) selectedCards.get(index);
+            if (player.getSun() >= plant.getSun() && plant.getPermissionTime() == 0) {
+                selected = plant;
+                System.out.println("select successfully");
+            } else {
+                System.out.println("you cant select this card");
+            }
+        }else {
+            System.out.println("invalid card name");
+            throw new NotPlantException();
+        }
+    }
+
+    @Override
+    public void select(int a) {
+
+    }
+
+    @Override
+    public void plant(int row, int column) throws noCardSelected, CloneNotSupportedException {
+
+    }
+
+    @Override
+    public void showLawn() {
+
+    }
+
+    @Override
+    public void endTurn() {
+
+    }
+
+    @Override
+    public void showSun() {
+        System.out.println(Game.accounts[0].getPlayer().getSun());
+    }
+
+    @Override
+    public void record() {
+
+    }
+
+    @Override
+    public void list() {
+
+    }
+
+    @Override
+    public void remove(int column, int row) {
+        Square square=map.board[row][column];
+        if(square.plant.size()!=0){
+            square.plant.remove(0);
+        }
+        else {
+            System.out.println("no plant is here");
+        }
+    }
+
+    @Override
+    public void handleWin() {
+
+    }
+
+    @Override
+    public void waveGenerate() {
+
+    }
+
+    @Override
+    public boolean isStart() {
+        return false;
+    }
+
+    @Override
+    public void setStart(boolean b) {
+
+    }
+
+    @Override
+    public void showLanes() {
+
+    }
+
+    @Override
+    public void put(String name, int number) throws CloneNotSupportedException, invalidCardExeption {
+
+    }
+
+
 }
